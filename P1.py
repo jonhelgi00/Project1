300d03ae0b1a17b8ac4bb04af5b30d37ba0f6945
jonhelgi00 <jonhelgi12@gmail.com>
2023-09-08 11:04:07 +0200

    sift image

--- P1.py
+++ P1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # read input image
 img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
@@ -7,7 +8,8 @@ img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.J
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Initiate SIFT object with default values
-sift = cv2.SIFT_create()
+# sift = cv2.SIFT_create()
+sift = cv2.xfeatures2d.SIFT_create() 
 
 # find the keypoints on image (grayscale)
 kp = sift.detect(gray,None)
@@ -16,6 +18,7 @@ kp = sift.detect(gray,None)
 img2 = cv2.drawKeypoints(gray, kp, None, flags=0)
 
 # display the image with keypoints drawn on it
+cv2.imwrite('KTH_SIFT1.jpg', img2)
 cv2.imshow("Keypoints", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
