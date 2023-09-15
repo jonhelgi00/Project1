e46b21a395dcd2e5396bef8b5eda73e89c9f2c56
jonhelgi00 <jonhelgi12@gmail.com>
2023-09-15 14:37:25 +0200

    SIFT part of 3 done

--- F_match.py
+++ F_match.py
@@ -28,23 +28,87 @@ def fixed_threshold():
   # Initialize the Brute-Force Matcher
   bf = cv2.BFMatcher()
 
+  max_radius = 50
+  # Match descriptors
+  matches = bf.radiusMatch(des, des_database, maxDistance = max_radius)
+  
+    
+  # good_matches = []
+  
+  # for m, n in matches:
+  #   if m.distance < 0.98 * n.distance:
+  #       good_matches.append([m])
+
+  matched_image = cv2.drawMatchesKnn(gray, kp, gray_database, kp_database, matches, None,
+          matchColor=(0, 255, 0), matchesMask=None,
+          singlePointColor=(255, 0, 0), flags=0)
+  
+  cv2.imwrite('KTH_SIFT_matching_r50_subopt.jpg', matched_image)
+  cv2.imshow("matches", matched_image)
+  cv2.waitKey(0)
+
+def NN():
+  img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
+  img_database = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_t1.JPG')
+
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray_database = cv2.cvtColor(img_database, cv2.COLOR_BGR2GRAY)
+
+  sift = cv2.xfeatures2d.SIFT_create(contrastThreshold=0.18, edgeThreshold = 100) 
+
+  kp, des = sift.detectAndCompute(gray, None)
+  kp_database, des_database = sift.detectAndCompute(gray_database, None)
+
+  bf = cv2.BFMatcher()
+
+  # Match descriptors
+  matches = bf.knnMatch(des, des_database, k=1)
+  
+  matched_image = cv2.drawMatchesKnn(gray, kp, gray_database, kp_database, matches, None,
+          matchColor=(0, 255, 0), matchesMask=None,
+          singlePointColor=(255, 0, 0), flags=0)
+  
+  cv2.imwrite('KTH_SIFT_matching_NN.jpg', matched_image)
+  cv2.imshow("matches", matched_image)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+
+def NNDR():
+  img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
+  img_database = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_t1.JPG')
+
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray_database = cv2.cvtColor(img_database, cv2.COLOR_BGR2GRAY)
+
+  sift = cv2.xfeatures2d.SIFT_create(contrastThreshold=0.18, edgeThreshold = 100) 
+
+  kp, des = sift.detectAndCompute(gray, None)
+  kp_database, des_database = sift.detectAndCompute(gray_database, None)
+
+  bf = cv2.BFMatcher()
+
   # Match descriptors
   matches = bf.knnMatch(des, des_database, k=2)
-  
+
   good_matches = []
+
+  ratio = 0.5
   
   for m, n in matches:
-    if m.distance < 0.98 * n.distance:
+    if m.distance < ratio * n.distance:
         good_matches.append([m])
-
+  
   matched_image = cv2.drawMatchesKnn(gray, kp, gray_database, kp_database, good_matches, None,
           matchColor=(0, 255, 0), matchesMask=None,
           singlePointColor=(255, 0, 0), flags=0)
   
+  cv2.imwrite('KTH_SIFT_matching_NNDR_0.5.jpg', matched_image)
   cv2.imshow("matches", matched_image)
   cv2.waitKey(0)
+  cv2.destroyAllWindows()  
 
-fixed_threshold()
+NNDR()  
 
 
 
