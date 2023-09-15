b8e96b9363b9c976c670a42075ea0579890570fb
jonhelgi00 <jonhelgi12@gmail.com>
2023-09-15 16:22:38 +0200

    SURFandSURF rotation complete

--- F_match.py
+++ F_match.py
@@ -14,8 +14,8 @@ def fixed_threshold():
   kp, des = sift.detectAndCompute(gray, None)
   kp_database, des_database = sift.detectAndCompute(gray_database, None)
 
-  # print(f"Number of keypoints in query image: {len(kp)}")
-  # print(f"Number of keypoints in database image: {len(kp_database)}")
+  print(f"Number of keypoints in query image: {len(kp)}")
+  print(f"Number of keypoints in database image: {len(kp_database)}")
 
   # img2_ = cv2.drawKeypoints(gray, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
   # img2_database = cv2.drawKeypoints(gray_database, kp_database, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -43,7 +43,7 @@ def fixed_threshold():
           matchColor=(0, 255, 0), matchesMask=None,
           singlePointColor=(255, 0, 0), flags=0)
   
-  cv2.imwrite('KTH_SIFT_matching_r50_subopt.jpg', matched_image)
+  # cv2.imwrite('KTH_SIFT_matching_r50_subopt.jpg', matched_image)
   cv2.imshow("matches", matched_image)
   cv2.waitKey(0)
 
@@ -93,7 +93,7 @@ def NNDR():
 
   good_matches = []
 
-  ratio = 0.5
+  ratio = 0.8
   
   for m, n in matches:
     if m.distance < ratio * n.distance:
@@ -103,18 +103,55 @@ def NNDR():
           matchColor=(0, 255, 0), matchesMask=None,
           singlePointColor=(255, 0, 0), flags=0)
   
-  cv2.imwrite('KTH_SIFT_matching_NNDR_0.5.jpg', matched_image)
-  cv2.imshow("matches", matched_image)
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()  
-
-NNDR()  
-
+  print(len(good_matches))
+  # cv2.imwrite('KTH_SIFT_matching_NNDR_0.5.jpg', matched_image)
+  # cv2.imshow("matches", matched_image)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()  
 
 
+def NNDR_SURF():
+  img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
+  img_database = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_t1.JPG')
+
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray_database = cv2.cvtColor(img_database, cv2.COLOR_BGR2GRAY)
+
+  # surf = cv2.xfeatures2d.SURF_create(5000) 
+  surf = cv2.xfeatures2d.SURF_create(6000) 
+
+  kp, des = surf.detectAndCompute(gray,None)
+  kp_database, des_database = surf.detectAndCompute(gray_database,None)
+  print(f"Number of keypoints in 1_5 is: {len(kp)}")
+  print(f"Number of keypoints in t1 is: {len(kp_database)}")
+
+  # kp, des = sift.detectAndCompute(gray, None)
+  # kp_database, des_database = sift.detectAndCompute(gray_database, None)
+
+  bf = cv2.BFMatcher()
+
+  # Match descriptors
+  matches = bf.knnMatch(des, des_database, k=2)
+
+  good_matches = []
+
+  ratio = 0.8
+  
+  for m, n in matches:
+    if m.distance < ratio * n.distance:
+        good_matches.append([m])
+  
+  matched_image = cv2.drawMatchesKnn(gray, kp, gray_database, kp_database, good_matches, None,
+          matchColor=(0, 255, 0), matchesMask=None,
+          singlePointColor=(255, 0, 0), flags=0)
+  
+  print(len(good_matches))
+  
+  # cv2.imwrite('KTH_SURF_matching_NNDR_0.8_new.jpg', matched_image)
+  # cv2.imshow("matches", matched_image)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()  
 
 
 
-
-
-
+NNDR()
--- P1.py
+++ P1.py
@@ -109,12 +109,91 @@ for angle in angle_list:
 # print(repeat_list)
 repeat_list = np.array(repeat_list)
 
+
+def SURF_rotation():
+  # Load the image
+  img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
+
+  #add padding to avoid cut-off
+  old_height, old_width, channels = img.shape
+
+  new_width = int(np.ceil(np.sqrt(old_height**2 + old_width**2)))
+  color = (0,0,0)
+  result = np.full((new_width, new_width, channels), color, dtype=np.uint8)
+
+  x_center = (new_width - old_width) // 2
+  y_center = (new_width - old_height) // 2
+
+  # copy img image into center of result image
+  result[y_center:y_center+old_height, 
+        x_center:x_center+old_width] = img
+
+  img_ = result
+
+  #grayscale
+  gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+
+  # Get the center of the image
+  height, width = gray.shape[:2]
+  # center = (width // 2, height // 2) #floored
+  center = (width / 2, height / 2)
+  surf = cv2.xfeatures2d.SURF_create(5000) 
+  kp_og = surf.detect(gray,None)
+  N_kp = len(kp_og)
+  img2_ = cv2.drawKeypoints(gray, kp_og, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+  # print(type(kp_og[0].pt[0]))
+  # cv2.circle(gray,(round(kp_og[200].pt[0]),round(kp_og[200].pt[1])), 30, (250,0,0), -1)
+  # cv2.imwrite('KTH_SIFT_padding_528kp.jpg', img2_)
+  # cv2.imshow('Keypoints in OG image (padded)', img2_)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
+
+  # Define the rotation angle (in degrees)
+  angle_list = np.arange(stop=360, step=15) # 0-345 degrees
+  match_counter = 0
+  repeat_list = []
+
+  for angle in angle_list:
+    # Calculate the rotation matrix
+    match_counter = 0
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    # Apply the rotation to the image
+    rotated_image = cv2.warpAffine(gray, rotation_matrix, (width, height), flags=cv2.INTER_CUBIC)
+    kp_rotated = surf.detect(rotated_image,None) #detect keypoints
+    kp_ideal = []
+    for kp in kp_og:
+      og_coord = np.array([[kp.pt[0],
+                          kp.pt[1]]])
+      rotation_matrix = np.array(rotation_matrix) #transform to np array
+      # kp_new = rotation_matrix[:,:2] @ og_coord.T + rotation_matrix[:,-1]
+      kp_new = rotation_matrix[:,:2] @ og_coord.T + np.reshape(rotation_matrix[:,-1],(2,1))
+      kp_new = np.reshape(kp_new, (2,))
+      kp_ideal.append(kp_new)
+      found = False
+      for kp_rot in kp_rotated:
+        rot_coord = kp_rot.pt
+        rot_coord = np.array(rot_coord)
+        if np.linalg.norm(kp_new - rot_coord) < 2 and not found:
+          match_counter += 1
+          found = True
+    repeatability = match_counter / N_kp
+    repeat_list.append((angle, repeatability))  
+    
+
+  # print(repeat_list)
+  repeat_list = np.array(repeat_list)
+  return repeat_list
+
+
+repeat_list_SURF = SURF_rotation()
 #Plot repeatability
 plt.figure(figsize=(8, 6))  # Adjust the figure size as needed.
 # Plot the data.
-plt.plot(repeat_list[:,0], repeat_list[:,1], label='rep', color='blue', linestyle='-')
+plt.plot(repeat_list[:,0], repeat_list[:,1], label='SIFT', color='blue', linestyle='-')
+plt.plot(repeat_list[:,0], repeat_list_SURF[:,1], label='SURF', color='red', linestyle='-')
 # Add a title.
-plt.title('Repeatability vs Rotation for SIFT')
+plt.title('Repeatability vs Rotation for SIFT & SURF')
 # Customize x and y labels.
 plt.xlabel('Rotation Angle (degrees)')
 plt.ylabel('Repeatability')
@@ -122,22 +201,10 @@ plt.ylabel('Repeatability')
 custom_xticks = [0,45,90,135, 180, 225, 270, 315]  # Replace with your custom xticks.
 plt.xticks(custom_xticks)
 # Add a legend if multiple lines are plotted.
-# plt.legend()
+plt.legend()
+plt.ylim((0, 1))
 # Show the plot.
 plt.show()
-
-
-
-def scalebla():
-  img = cv2.imread('/Users/jonhelgi/KTH_projects/Analysis_Search/Project1/obj1_5.JPG')
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  img_scaled = cv2.resize(gray, None, fx = 2, fy = 2, interpolation=cv2.INTER_CUBIC)
-  cv2.imshow('Scaled', img_scaled)
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
-
-# scalebla()
-
 
 
 
